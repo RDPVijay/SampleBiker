64a788370b9d4148469722b1bf4e778763ba23ea
Ralph Didi Vijay <rdv.vijay@gmail.com>
2016-03-29 17:11:34 +0530

    #6 - Changed Listadapter with JSON parsing in List View <Ralph>

--- app/src/main/java/com/example/lenovo/samplebiker/OrderList.java
+++ app/src/main/java/com/example/lenovo/samplebiker/OrderList.java
@@ -1,24 +1,36 @@
 package com.example.lenovo.samplebiker;
 
 import android.app.ListActivity;
-import android.content.Intent;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
 
 
-public class OrderList extends ListActivity implements ListAdapter.customButtonListener {
+public class OrderList extends ListActivity {
+
+    private ProgressDialog progress;
+    ArrayList<HashMap<String, String>> orderlist;
+    JSONArray order = null;
 
 
-    private ListView listView;
-    ListAdapter adapter;
-    ArrayList<String> dataItems = new ArrayList<String>();
-    ArrayList<String> dataItems2 = new ArrayList<String>();
-
+    private static final String TAG_MESSAGES = "messages";
+    private static final String TAG_ID = "id";
+    private static final String TAG_FROM = "from";
+    private static final String TAG_EMAIL = "email";
+    private static final String TAG_SUBJECT = "subject";
+    private static final String TAG_DATE = "date";
 
 
     public void onCreate(Bundle savedInstanceState){
@@ -27,32 +39,95 @@ public class OrderList extends ListActivity implements ListAdapter.customButtonL
 
         setContentView(R.layout.oder_list);
 
+        orderlist = new ArrayList<HashMap<String, String>>();
 
-        String[] Source = getResources().getStringArray(R.array.Source);
-        List<String> dataTemp = Arrays.asList(Source);
-        dataItems.addAll(dataTemp);
-
-        String[] Dest = getResources().getStringArray(R.array.Dest);
-        List<String> dataTemp2 = Arrays.asList(Dest);
-        dataItems2.addAll(dataTemp2);
-
-        listView = (ListView)findViewById(android.R.id.list);
-        adapter = new ListAdapter(OrderList.this, dataItems,dataItems2);
-        adapter.setCustomButtonListener(OrderList.this);
-        listView.setAdapter(adapter);
+        new LoadInbox().execute();
 
     }
 
-    @Override
-    public void onButtonClickListener(int position, String value, String value2) {
 
-        Toast.makeText(OrderList.this, "Map to " + value2, Toast.LENGTH_SHORT).show();
+    private class LoadInbox extends AsyncTask<String, String, String> {
 
-        Intent i = new Intent(this, OrderDetails.class);
-        i.putExtra("Sour", value);
-        i.putExtra("Dest", value2);
-        startActivity(i);
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progress = new ProgressDialog(OrderList.this);
+            progress.setMessage("Loading Orders ...");
+            progress.setIndeterminate(false);
+            progress.setCancelable(false);
+            progress.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+
+            JSONObject json = null;
+            try {
+                json = new JSONObject(loadJSONFromAsset());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Log.d("Inbox JSON: ", json.toString());
+
+            try {
+                order = json.getJSONArray(TAG_MESSAGES);
+                // looping through All messages
+                for (int i = 0; i < order.length(); i++) {
+                    JSONObject c = order.getJSONObject(i);
+
+                    String id = c.getString(TAG_ID);
+                    String from = c.getString(TAG_FROM);
+                    String subject = c.getString(TAG_SUBJECT);
+                    String date = c.getString(TAG_DATE);
+
+                    HashMap<String, String> map = new HashMap<String, String>();
+
+                    map.put(TAG_ID, id);
+                    map.put(TAG_FROM, from);
+                    map.put(TAG_SUBJECT, subject);
+                    map.put(TAG_DATE, date);
+
+                    orderlist.add(map);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        private String loadJSONFromAsset() {
+            String json = null;
+            try {
+                InputStream is = getAssets().open("Order.json");
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+                json = new String(buffer, "UTF-8");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return null;
+            }
+            return json;
+        }
+
+        protected void onPostExecute(String file_url) {
+            progress.dismiss();
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    ListAdapter adapter = new SimpleAdapter(
+                            OrderList.this, orderlist,
+                            R.layout.order_list_item, new String[] { TAG_FROM, TAG_SUBJECT, TAG_ID },
+                            new int[] { R.id.Dest, R.id.Source, R.id.checkBox });
+                    setListAdapter(adapter);
+                }
+            });
+
+        }
 
     }
-
 }
